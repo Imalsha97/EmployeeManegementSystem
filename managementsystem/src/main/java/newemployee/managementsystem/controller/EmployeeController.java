6ec9a34bae503df5ac7a89670d0c978ee79791e9
Imalsha <imalsham092@gmail.com>
2021-09-13 14:42:14 -0700

    backend rest api get and post

---
+++ managementsystem/src/main/java/newemployee/managementsystem/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package newemployee.managementsystem.controller;
+
+import newemployee.managementsystem.model.Employee;
+import newemployee.managementsystem.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@CrossOrigin(origins = "http://localhost:3000/")
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    //create employee rest api
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+
+    }
+
+}
---
+++ managementsystem/src/test/java/newemployee/managementsystem/ManagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package newemployee.managementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
